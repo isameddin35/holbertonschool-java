58a20b372a998024781f47e928138eb66ecc7fe6
Isameddin <isaqurbanov112@gmail.com>
2025-11-13 22:54:18 +0400

    firstcommit

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,7 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton ";
+        String text2 = "School";
+        System.out.println("Welcome to " + text1 + text2 + " !");
+    }
+}
