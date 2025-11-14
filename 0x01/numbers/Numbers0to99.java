e0554daa36c7c23bdbb41ed6d4280755c599b4da
Isameddin <isaqurbanov112@gmail.com>
2025-11-14 13:59:17 +0400

    numbers

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,10 @@
+package numbers;
+
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + ", ");
+        }
+        System.out.println(100);
+    }
+}
