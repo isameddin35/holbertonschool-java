bb4d45e361a633c0f1a51170f61146c08d84bc51
Isameddin <isaqurbanov112@gmail.com>
2025-11-14 14:04:14 +0400

    numbers

---
+++ 0x01/even/EvenNumbers.java
@@ -0,0 +1,10 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println(98);
+    }
+}
