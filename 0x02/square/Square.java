5c187f3652968df1fabb6ddf1b1085a6bf79194b
isa <isaqurbanov112@gmail.com>
2025-11-20 02:39:40 +0400

    exception

--- 0x02/square/Square.java
+++ 0x02/square/Square.java
@@ -2,7 +2,7 @@ public class Square {
     public static double area(double side) {
         double area = 0;
         if (side < 0) {
-            throw new IllegalArgumentException("Side must have a positive value.");
+            throw new IllegalArgumentException("Side must have a positive value");
         } else {
             area = side * side;
         }
