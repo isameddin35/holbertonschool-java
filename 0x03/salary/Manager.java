253c6c415db65a6e2da3326ed9ebb2fa9b38aaf0
isa <isaqurbanov112@gmail.com>
2025-11-20 20:08:52 +0400

    salary

--- 0x03/salary/Manager.java
+++ 0x03/salary/Manager.java
@@ -7,7 +7,7 @@ public class Manager extends Employee {
     public double calculateBonus(Department department) {
         double bonus = 0;
         if (department.reachedTarget()) {
-            bonus = this.getFixedSalary() / 5;
+            bonus = this.getFixedSalary() / 5 + ((department.getAchievedTargetValue() - department.getTargetValue())/100);
         }
         return bonus;
     }
