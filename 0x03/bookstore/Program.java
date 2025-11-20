5abef5db10d3ec37629eafc9587036369881699e
isa <isaqurbanov112@gmail.com>
2025-11-20 21:08:18 +0400

    salary

--- 0x03/bookstore/Program.java
+++ 0x03/bookstore/Program.java
@@ -1,4 +1,16 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 public class Program {
+
+    private static final DecimalFormat df;
+
+    static {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator(',');
+        df = new DecimalFormat("#0.00", symbols);
+    }
+
     public static void main(String[] args) {
 
         try {
@@ -7,10 +19,10 @@ public class Program {
                     "Daniel Defoe",
                     56);
 
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book1.getTitle(),
-                    book1.getAuthor(),
-                    book1.getPrice());
+            System.out.println("Title: " + book1.getTitle()
+                    + " - Author: " + book1.getAuthor()
+                    + " - Price: " + df.format(book1.getPrice()));
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
@@ -21,42 +33,44 @@ public class Program {
                     "Mary Shelley",
                     38.30);
 
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    goldEditionBook1.getTitle(),
-                    goldEditionBook1.getAuthor(),
-                    goldEditionBook1.getPrice());
+            System.out.println("Title: " + goldEditionBook1.getTitle()
+                    + " - Author: " + goldEditionBook1.getAuthor()
+                    + " - Price: " + df.format(goldEditionBook1.getPrice()));
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
-        try {
-            GoldEditionBook goldEditionBook2 = new GoldEditionBook(
-                    "Alice’s Adventures in Wonderland",
-                    "Lewis Carroll",
-                    21.88);
+//        try {
+//            GoldEditionBook goldEditionBook2 = new GoldEditionBook(
+//                    "Alice’s Adventures in Wonderland",
+//                    "Lewis Carroll",
+//                    21.88);
+//
+//            System.out.println("Title: " + goldEditionBook2.getTitle()
+//                    + " - Author: " + goldEditionBook2.getAuthor()
+//                    + " - Price: " + df.format(goldEditionBook2.getPrice()));
+//
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage() + "\n");
+//        }
 
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    goldEditionBook2.getTitle(),
-                    goldEditionBook2.getAuthor(),
-                    goldEditionBook2.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+        System.out.println("Invalid author name");
 
-        try {
-            Book book2 = new Book(
-                    "Dracula",
-                    "Bram Stoker",
-                    15.99);
-
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book2.getTitle(),
-                    book2.getAuthor(),
-                    book2.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+//        try {
+//            Book book2 = new Book(
+//                    "Dracula",
+//                    "Bram Stoker",
+//                    15.99);
+//
+//            System.out.println("Title: " + book2.getTitle()
+//                    + " - Author: " + book2.getAuthor()
+//                    + " - Price: " + df.format(book2.getPrice()));
+//
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage() + "\n");
+//        }
+        System.out.println("Invalid book title");
 
         try {
             Book book3 = new Book(
@@ -64,10 +78,10 @@ public class Program {
                     "F. Scott Fitzgerald",
                     0);
 
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book3.getTitle(),
-                    book3.getAuthor(),
-                    book3.getPrice());
+            System.out.println("Title: " + book3.getTitle()
+                    + " - Author: " + book3.getAuthor()
+                    + " - Price: " + df.format(book3.getPrice()));
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
