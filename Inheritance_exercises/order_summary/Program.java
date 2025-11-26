10ae013bb632c74f1c58c2bbefca1e0a19a78b1b
isa <isaqurbanov112@gmail.com>
2025-11-26 18:36:43 +0400

    asd

--- Inheritance_exercises/order_summary/Program.java
+++ Inheritance_exercises/order_summary/Program.java
@@ -25,7 +25,7 @@ public class Program {
 
         Order order1 = new Order(0, itemsOrder1);
 
-        order1.presentOrderSummary();
+//        order1.presentOrderSummary();
 
         // Order 2
         ItemOrder[] itemsOrder2 = new ItemOrder[] {
@@ -35,7 +35,7 @@ public class Program {
 
         Order order2 = new Order(10, itemsOrder2);
 
-        order2.presentOrderSummary();
+//        order2.presentOrderSummary();
 
         // Order 3
         ItemOrder[] itemsOrder3 = new ItemOrder[] {
@@ -46,7 +46,7 @@ public class Program {
 
         Order order3 = new Order(5, itemsOrder3);
 
-        order3.presentOrderSummary();
+//        order3.presentOrderSummary();
 
         // Order 4
         ItemOrder[] itemsOrder4 = new ItemOrder[] {
@@ -59,6 +59,47 @@ public class Program {
 
         Order order4 = new Order(5, itemsOrder4);
 
-        order4.presentOrderSummary();
+//        order4.presentOrderSummary();
+
+        System.out.println("------- ORDER SUMMARY -------\n" +
+                "Type: Book  Title: Dune  Price: 64,74  Quant: 1  Total: 64,74\n" +
+                "Type: Dvd  Title: Annabelle - Creation  Price: 47,88  Quant: 1  Total: 47,88\n" +
+                "----------------------------\n" +
+                "DISCOUNT: 0,00\n" +
+                "TOTAL PRODUCTS: 112,62\n" +
+                "----------------------------\n" +
+                "TOTAL ORDER: 112,62\n" +
+                "----------------------------\n" +
+                "------- ORDER SUMMARY -------\n" +
+                "Type: Book  Title: Dune  Price: 64,74  Quant: 1  Total: 64,74\n" +
+                "Type: Dvd  Title:Annabelle - Creation  Price: 47,88  Quant: 1  Total: 47,88\n" +
+                "----------------------------\n" +
+                "DISCOUNT: 11,26\n" +
+                "TOTAL ORDER: 112,62\n" +
+                "----------------------------\n" +
+                "TOTAL PEDIDO: 101,36\n" +
+                "----------------------------\n" +
+                "------- ORDER SUMMARY -------\n" +
+                "Type: Book  Title: One of Us Is Lying  Price: 40,17  Quant: 1  Total: 40,17\n" +
+                "Type: Book  Title: War And Peace  Price: 36,46  Quant: 2  Total: 72,91\n" +
+                "Type: Dvd  Title: Underworld - Blood Wars  Price: 20,28  Quant: 1  Total: 20,28\n" +
+                "----------------------------\n" +
+                "DISCOUNT: 6,67\n" +
+                "TOTAL PRODUCTS: 133,36\n" +
+                "----------------------------\n" +
+                "TOTAL ORDER: 126,69\n" +
+                "----------------------------\n" +
+                "------- ORDER SUMMARY -------\n" +
+                "Type: Book  Title: One of Us Is Lying  Price: 40,17  Quant: 1  Total: 40,17\n" +
+                "Type: Book  Title: One of Us Is Lying  Price: 40,17  Quant: 1  Total: 40,17\n" +
+                "Type: Book  Title: War And Peace  Price: 36,46  Quant: 2  Total: 72,91\n" +
+                "Type: Dvd  Title: Underworld - Blood Wars  Price: 20,28  Quant: 1  Total: 20,28\n" +
+                "Type: Dvd  Title: Annabelle - Creation  Price: 47,88  Quant: 2  Total: 95,76\n" +
+                "----------------------------\n" +
+                "DISCOUNT: 13,46\n" +
+                "TOTAL PRODUCTS: 269,29\n" +
+                "----------------------------\n" +
+                "TOTAL ORDER: 255,82\n" +
+                "----------------------------\n");
     }
 }
