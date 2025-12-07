7b436751d3cac93ec134ad28e0d0a47801f13f27
isa <isaqurbanov112@gmail.com>
2025-12-07 11:00:25 +0400

    asd

--- List/todo/Task.java
+++ List/todo/Task.java
@@ -10,7 +10,7 @@ public class Task {
 
     public void modifyDescription(String newDescription) throws Exception {
         if (newDescription == null || newDescription.isEmpty()) {
-            throw new IllegalArgumentException("Invalid task description");
+            throw new Exception("Invalid task description");
         } else {
             this.description = newDescription;
         }
